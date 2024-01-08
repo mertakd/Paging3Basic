4df874182abc7b59280c98152bef66a84f5f6b08
Mert Akdoğan2 <65607835+mertakd@users.noreply.github.com>
2024-01-08 14:18:47 +0300

    Paging Source Added

--- app/src/main/java/com/example/android/codelabs/paging/data/ArticlePagingSource.kt
+++ app/src/main/java/com/example/android/codelabs/paging/data/ArticlePagingSource.kt
@@ -1,2 +1,58 @@
 package com.example.android.codelabs.paging.data
 
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import java.time.LocalDateTime
+import kotlin.math.max
+
+
+private const val STARTING_KEY = 0
+private const val LOAD_DELAY_MILLIS = 3_000L
+
+private val firstArticleCreatedTime = LocalDateTime.now()
+class ArticlePagingSource : PagingSource<Int, Article>() {
+
+    private fun ensureValidKey(key: Int) = max(STARTING_KEY, key)
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
+        // Bu ilk yüklemeyse sayfalamayı STARTING_KEY ile başlatın
+        val start = params.key ?: STARTING_KEY
+        // params.loadSize tarafından belirtilen sayıda öğe yükleyin
+        val range = start.until(start + params.loadSize)
+
+        //Sonuç başarılıysa LoadResult.Page
+        return LoadResult.Page(
+            data = range.map { number ->
+                Article(
+                    // Makale kimliği olarak ardışık artan sayılar oluşturun
+                    id = number,
+                    title = "Article $number",
+                    description = "This describes article $number",
+                    created = firstArticleCreatedTime.minusDays(number.toLong())
+                )
+            },
+
+            // Öğeleri STARTING_KEY'in arkasına yüklemeye çalışmadığımızdan emin olun
+            prevKey = when (start) {
+                STARTING_KEY -> null
+                else -> ensureValidKey(key = range.first - params.loadSize)
+            },
+            nextKey = range.last + 1
+        )
+    }
+
+
+
+
+
+
+    override fun getRefreshKey(state: PagingState<Int, Article>): Int? {
+        val anchorPosition = state.anchorPosition ?: return null //state.anchorPosition ile UI'nin anlık konumunu alır.
+        val article = state.closestItemToPosition(anchorPosition) ?: return null // state.closestItemToPosition(anchorPosition) ile anchorPosition'a en yakın öğeyi alır
+        return ensureValidKey(key = article.id - (state.config.pageSize / 2)) // ensureValidKey fonksiyonu, geçerli bir anahtarı sağlamak için kullanılır
+    }
+
+    /*
+    * Şu anki UI'nin belirli bir konumundaki makaleye yakın olan makalenin id'sini alır ve bu id'yi kullanarak yeni bir PagingSource'un başlaması gereken anahtar değerini belirler. Bu, kullanıcının sayfalama işleminden sonra listenin başka bir sayfaya geçmesini önlemeye yardımcı olur ve daha tutarlı bir kullanıcı deneyimi sağlar.
+    * */
+}
